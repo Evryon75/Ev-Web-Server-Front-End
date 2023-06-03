66452f9fc27c343b4c18e6de57570fa53d1141b4
Evryon <fireden70@gmail.com>
2023-06-03 13:48:14 +0200

    SO BALLER

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import {Component, signal} from '@angular/core';
+import {Test} from "./models.module";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // @ts-ignore
+  brother: Test = {};
+  title: string = "brother";
+  constructor(public http: HttpClient, private router: Router) {
+    this.http.get<Test>('http://127.0.0.1:448/test').subscribe(d => {
+      this.brother = d;
+    });
+  }
+}
